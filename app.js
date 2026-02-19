5017796cd87f9343703326040d372081b64a9be2
Dhayalan14 <Dhayalan14@users.noreply.github.com>
2026-02-19 05:26:10 +0000

    deploy: b3e458aae81f0fbbe9b09a43d314a552f23aa209

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 // Configuration & State
 let state = {
-    apiKey: 'AIzaSyD5S9TFQuLx2zUNB2h9CnuhpPsDft9SKX0',
+    apiKey: 'GOOGLE_API_KEY',
     isConnected: false,
     isListening: false,
     ws: null,
@@ -413,4 +413,5 @@ function stopSession() {
     if (state.ws) state.ws.close();
     if (state.stream) state.stream.getTracks().forEach(t => t.stop());
 }
+
 
